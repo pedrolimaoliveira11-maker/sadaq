1d85c0145c58e5675178cf951a416d7106c206e6
pedrolimaoliveira11-maker <pedro.lima.oliveira11@escola.pr.gov.br>
2026-03-25 09:19:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let vendas = JSON.parse(localStorage.getItem('db_vendas_spfc')) || [];
+document.getElementById('form-cliente').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const nome = document.getElementById('nome-cliente').value.trim();
+    if(nome) {
+        clientes.push(nome);
+        localStorage.setItem('db_clientes_spfc', JSON.stringify(clientes));
+        atualizarClientes();
+        document.getElementById('form-cliente').reset();
+    }
+});
