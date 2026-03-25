5caa42e3c22e4505ba9c8a6601979bcf5360b388
pedrolimaoliveira11-maker <pedro.lima.oliveira11@escola.pr.gov.br>
2026-03-25 09:28:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-function atualizarVendas() {
-    const corpo = document.getElementById('corpo-tabela-vendas');
-    corpo.innerHTML = '';
-    let total = 0;
-    vendas.forEach(v => {
-        corpo.innerHTML += `<tr>
-            <td>${v.data}</td>
-            <td>${v.cliente}</td>
-            <td>${v.produto}</td>
-            <td>R$ ${v.preco.toFixed(2)}</td>
-        </tr>`;
-        total += v.preco;
-    });
-    document.getElementById('total-caixa').innerText = `Total em Caixa: R$ ${total.toFixed(2)}`;
-}
+window.onload = () => {
+    atualizarClientes();
+    atualizarVendas();
+};
