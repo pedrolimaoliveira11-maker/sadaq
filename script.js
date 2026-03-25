2dde05470379810656f7596012361f6ea187681a
pedrolimaoliveira11-maker <pedro.lima.oliveira11@escola.pr.gov.br>
2026-03-25 09:23:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-function atualizarClientes() {
-    const lista = document.getElementById('lista-clientes-simples');
-    const select = document.getElementById('select-venda-cliente');
-    lista.innerHTML = '';
-    select.innerHTML = '<option value="">Selecione um cliente...</option>';
-    clientes.forEach((c, i) => {
-        lista.innerHTML += `<tr><td>${c}</td><td><button onclick="removerCliente(${i})">Remover</button></td></tr>`;
-        select.innerHTML += `<option value="${c}">${c}</option>`;
-    });
+function removerCliente(index) {
+    clientes.splice(index, 1);
+    localStorage.setItem('db_clientes_spfc', JSON.stringify(clientes));
+    atualizarClientes();
 }
-
